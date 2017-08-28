25afbc67ce5640acfd2d562f60ac007dde4b1919
efrain coello <carlosefrain17@hotmail.com>
2017-08-27 19:47:39 -0500

    clear arquitecture del backend

---
+++ server/server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var express = require('express');
+var bodyparser = require('body-parser');
+
+var db_tools = require('./tools/db_tools');
+
+var app = express();
+
+
+db_tools.DBConnectMongoose()
+    .then(() => {
+        var routes = require('./routes/routes');
+
+        // configure app to use bodyParser()
+        // this will let us get the data from a POST
+        app.use(bodyparser.urlencoded({extended: true}));
+        app.use(bodyparser.json({limit: '10mb'}));
+
+        routes.assignRoutes(app);
+
+        app.listen(3000);
+
+        console.log('Server listening on port 3000');
+    })
+    .catch(err => {
+        console.log('Error: ' + err)
+    })
